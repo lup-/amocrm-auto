f4c5f0d3d71253b7bf0142a9a829ddccd3af03f6
Dima Nikolaev <dmitriy.nikolaev1337@gmail.com>
2019-09-30 21:11:05 +0500

    Календарь с прямыми ссылками на события в браузере

--- public_html/script.js
+++ public_html/script.js
@@ -1,13 +1,13 @@
-function setupFullCalendar(instructorId) {
+document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid', 'interaction', 'timeGrid', 'list', 'googleCalendar'],
+        plugins: ['interaction', 'dayGrid', 'timeGrid', 'list', 'googleCalendar'],
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,listYear,timeGridWeek,timeGridDay'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listYear'
         },
+        locale: 'ru',
         editable: true,
         evenLimit: true,
         displayEventTime: false,
@@ -15,17 +15,28 @@ function setupFullCalendar(instructorId) {
         eventResizableFromStart: true,
         eventDurationEditable: true,
         eventClick: function (arg) {
+            // opens events in a popup window
             window.open(arg.event.url, '_blank', 'width=1000,height=600');
+            // prevents current tab from navigating
             arg.jsEvent.preventDefault();
         },
-        events: '/calendar.php?action=listEvents&instructorId='+instructorId,
+        // eventDragStart: function (info) {
+
+        //   var start = info.event.start.toISOString().replace('.000Z', '').replace('T', ' ');
+        //   console.log(start);
+        //   var end = info.event.end.toISOString().replace('.000Z', '').replace('T', ' ');
+        //   $.ajax({
+        //     url: "http://amo-auto.humanistic.tech/calendar.php?action=update&instructorId=920531&id=" + info.event.id + "&start=" + start + "&end=" + end
+        //   })
+        // },
+        events: 'http://amo-auto.humanistic.tech/calendar.php?action=listEvents&instructorId=920531',
         eventDrop: function (info) {
             var start = info.event.start.toISOString().replace('.000Z', '').replace('T', ' ');
             var end = info.event.end.toISOString().replace('.000Z', '').replace('T', ' ');
             $.ajax({
-                url: "/calendar.php?action=update&instructorId=" + instructorId + "&id=" + info.event.id + "&start=" + start + "&end=" + end
+                url: "http://amo-auto.humanistic.tech/calendar.php?action=update&instructorId=920531&id=" + info.event.id + "&start=" + start + "&end=" + end
             })
         },
     });
     calendar.render();
-}
+});
